262d05b2185420ad6d798ba1f45b68b0590e0bfa
Daniel Sánchez <dscruz.dev@gmail.com>
2024-05-22 18:34:20 +0200

    Add Moon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-</style>
+<div
+	class="flex h-screen items-center justify-center bg-cover bg-no-repeat"
+	style="background-image: url(moon.webp)"
+>
+	<div
+		class="flex h-full w-full flex-col items-center items-center justify-center gap-6 bg-transparent font-bold text-slate-200 backdrop-blur-md"
+	>
+		<h1 class="text-8xl">Daniel's Lair</h1>
+		<h2 class="text-4xl">Where cool things happen</h2>
+	</div>
+</div>
